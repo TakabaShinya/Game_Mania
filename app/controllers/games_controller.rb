cd797d32e74dfc6cab7977579205bb557949b825
EC2 Default User <ec2-user@ip-172-31-95-166.ec2.internal>
2023-01-12 13:52:42 +0000

    ユーザー編集とユーザー一覧を追加

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,38 @@
 class GamesController < ApplicationController
   def index
-  end
-
-  def show
+    @user = current_user
+    @game = Game.new
+    @games = Game.all
   end
 
   def create
+    @user = current_user
+    @games = Game.all
+    @game = Game.new(game_params)
+    @game.user_id = current_user.id
+    if @game.save
+      flash[:notice] = "新規投稿されました！"
+      redirect_to game_path(@game.id)
+    else
+      render :index
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    @user = @game.user
+    @game_new = Game.new
   end
 
   def destroy
+    game = Game.find(params[:id])
+    game.destroy
+    redirect_to games_path
+  end
+  
+  private
+  
+  def game_params
+    params.require(:game).permit(:title, :body, :user_id)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'bd48c2961868dc9907341fe530b3684964fc0f85275ec9539de96b66af5d58a9ca32c5f247c2c1d4e3e92e5ec29de9d621dd003b1cdc0f817d60af4a7a22248c'
