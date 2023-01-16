67d5dbfa45bae867f18b7ad77131e02b6ab59ae7
EC2 Default User <ec2-user@ip-172-31-95-166.ec2.internal>
2023-01-16 14:03:15 +0000

    ゲーム機種セレクト実装

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,6 @@ class Game < ApplicationRecord
   attachment :game_image
   belongs_to :user
   validates :title, presence: true
+  validates :hard, presence: true
   validates :body, presence: true
 end
